63573dc1e29f96322720c065d6ab24b8d0b69b90
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2025-02-11 00:07:33 +0530

    basic version of React.createElement()

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("hello there");
+import { render } from "./virtualDom.js";
+
+let btn = document.getElementById("btn");
+
+const vNode = {
+  type: "button",
+  props: {
+    id: "heading",
+    onclick: () => console.log("hello there"),
+  },
+  children: "Print something",
+};
+
+render(vNode);
+console.log("rendered dom");
---
+++ src/virtualDom.js
@@ -0,0 +1,40 @@
+export function VirtualNode(type, props, children) {
+  return {
+    type,
+    props: props || {},
+    // children: children.flat || [],
+    children: children,
+  };
+}
+
+export function render(virtualNode) {
+  const element = document.createElement(virtualNode.type);
+
+  let { props, children } = virtualNode;
+
+  console.log(virtualNode);
+
+  Object.entries(props).forEach(([key, val]) => {
+    if (key.startsWith("on") && typeof val == "function") {
+      element.addEventListener(key.slice(2).toLowerCase(), val);
+    } else {
+      element.setAttribute(key, val);
+    }
+  });
+
+  // children.forEach((child) => {
+  //   if (typeof child == "string" || typeof child == "number") {
+  //     const textNode = document.createTextNode(child);
+  //     element.appendChild(textNode);
+  //   } else {
+  //     render(child);
+  //   }
+  // });
+
+  // we assume that there is a single child
+  const textNode = document.createTextNode(children);
+  element.appendChild(textNode);
+
+  const app = document.getElementById("app");
+  app.appendChild(element);
+}
